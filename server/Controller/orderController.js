9eb34a92db4bd90cc6da10bfa51fd15a72a5f795
--replace-all <demola.hussainin@gmail.com>
2018-09-13 21:30:44 +0100

    [Feature #160477788] Add controller to handle creating an order

--- server/Controller/orderController.js
+++ server/Controller/orderController.js
@@ -8,6 +8,29 @@ class OrderController {
       mealsOrdered: orderModel,
     });
   }
+
+  static createOrder(req, res) {
+    const currentNumberOfOrders = orderModel.length;
+    const orderToCreate = req.body;
+    const orderCreated = {
+      id: currentNumberOfOrders + 1,
+      name: orderToCreate.name,
+      meal: orderToCreate.meal,
+      quantity: orderToCreate.quantity,
+      drink: orderToCreate.drink,
+      prize: orderToCreate.prize,
+      address: orderToCreate.address,
+    };
+    new Promise((resolve) => {
+      orderModel.push(orderCreated);
+      if (orderModel.length > currentNumberOfOrders) resolve();
+    }).then(() => {
+      res.jsend.success({
+        code: 200,
+        message: 'Order created succesfully',
+      });
+    }).catch(() => res.status(500).jsend.fail({ message: 'Order could not be created' }));
+  }
 }
 
 export default OrderController;
