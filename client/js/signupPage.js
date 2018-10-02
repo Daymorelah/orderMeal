775704863817e960c5fab2eb880e203d8754eb32
Daymorelah <demola.hussainin@gmail.com>
2018-10-02 05:16:03 +0100

    [Feature #160886027]Power signup page with backend
    - connect front-end and back-end
    - Update contents of front-end with back-end

---
+++ client/js/signupPage.js
@@ -0,0 +1,58 @@
+const signupFormContainer = document.querySelector('#signup-form');
+const signupForm = document.querySelector('form');
+const loginButton = document.querySelector('#login-button');
+const responseContainer = document.querySelector('#response-container');
+const responseContent = document.querySelector('#response-container p');
+/* eslint-disable no-else-return */
+const handleResponse = res => res.json().then((response) => {
+  if (res.ok) {
+    return response;
+  } else {
+    return Promise.reject(Object.assign({}, response, {
+      status: res.status,
+      statusText: res.statusText,
+    }));
+  }
+});
+/* eslint-disable no-else-return */
+const showErrorMessage = (res, type) => {
+  responseContainer.style.display = 'block';
+  responseContainer.setAttribute('class', `${type}-response`);
+  responseContent.textContent = `${res.data.message}!`;
+};
+signupForm.addEventListener('submit', (event) => {
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+  const email = document.querySelector('#email').value;
+  const userDetails = {
+    username,
+    password,
+    email,
+  };
+  event.preventDefault();
+  fetch(
+    'http://localhost:2022/api/v1/auth/signup',
+    {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  ).then(handleResponse).then((res) => {
+    localStorage.setItem('token', `${res.data.token}`);
+    showErrorMessage(res, 'success');
+    signupFormContainer.style.display = 'none';
+    signupForm.reset();
+    setTimeout(() => {
+      window.location = './userProfile.html';
+    }, 2500);
+  })
+    .catch((error) => {
+      showErrorMessage(error, 'error');
+    });
+});
+
+loginButton.addEventListener('click', () => {
+  window.location = './loginPage.html';
+});
