05031672243cea6180edb00b8b2544594127640d
Ademola Hussain <demola.hussainin@gmail.com>
2019-07-21 07:56:40 +0100

    Fix google auth redirect-not-found error Set index route to greet user to the API

--- server/strategies.js
+++ server/strategies.js
@@ -6,7 +6,7 @@ const GoogleStrategy = googleAuth.Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/api/v1/auth/callback',
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
 }, (accessToken, refreshToken, profile, callback) => callback(null, profile)));
 
 export default passport;
